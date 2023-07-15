061f5d0fb768872603a2e4bcc237ac822d0da477
Juan Camilo Medina <jmedi009@gmail.com>
2023-07-15 11:40:40 -0500

    Adding blog list view and templates

--- blogging/views.py
+++ blogging/views.py
@@ -1,4 +1,6 @@
-from django.http import HttpResponse, HttpResponseRedirect, Http404
+from django.shortcuts import render
+from django.http import HttpResponse
+from blogging.models import Post
 
 # Create your views here.
 def stub_view(request, *args, **kwargs):
@@ -10,3 +12,9 @@ def stub_view(request, *args, **kwargs):
         body += "Kwargs:\n"
         body += "\n".join(["\t%s: %s" % i for i in kwargs.items()])
     return HttpResponse(body, content_type="text/plain")
+
+def list_view(request):
+    published = Post.objects.exclude(published_date__exact=None)
+    posts = published.order_by('-published_date')
+    context = {'posts': posts}
+    return render(request, 'blogging/list.html', context)
