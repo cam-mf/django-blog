3db2e0981f4bf82cbaafb54ecad6e12c64534bc2
Juan Camilo Medina <jmedi009@gmail.com>
2023-07-30 23:52:43 -0500

    Fixing black

--- blogging/migrations/0004_merge_20230730_2346.py
+++ blogging/migrations/0004_merge_20230730_2346.py
@@ -4,11 +4,9 @@ from django.db import migrations
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('blogging', '0003_auto_20230730_2306'),
-        ('blogging', '0003_auto_20230730_1944'),
+        ("blogging", "0003_auto_20230730_2306"),
+        ("blogging", "0003_auto_20230730_1944"),
     ]
 
-    operations = [
-    ]
+    operations = []
